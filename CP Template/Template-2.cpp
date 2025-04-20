42fe0d807f4bb85ecc04f038776c0f83299c0987
Md. Abdur Rahim <raahimm11@gmail.com>
2025-04-20 10:00:22 +0600

    Added another CP template

---
+++ CP Template/Template-2.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std ;
+#define endl '\n' 
+
+void solve(){
+}
+
+int main() {
+	int t ; cin >> t ;
+	//t = 1 ;
+	while(t--) {
+		solve() ;
+	}
+	return 0 ;
+}
