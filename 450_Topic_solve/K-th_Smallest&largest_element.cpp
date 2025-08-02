8b45e5f2d98a9ed5faf84d47a72ba677d7d11aa8
Md. Abdur Rahim <raahimm11@gmail.com>
2025-08-02 08:16:27 +0600

    Updated Prorblem solve

--- 450_Topic_solve/K-th_Smallest&largest_element.cpp
+++ 450_Topic_solve/K-th_Smallest&largest_element.cpp
@@ -16,3 +16,15 @@ class Solution{
         return num ;
     }
 };
+
+/*
+    Problem link: https://www.naukri.com/code360/problems/kth-smallest-and-largest-element-of-array_1115488?topList=love-babbar-dsa-sheet-problems&utm_source=website&utm_medium=affiliate&utm_campaign=450dsatracker&count=25&page=9&search=&sort_entity=order&sort_order=ASC&leftPanelTabValue=SUBMISSION
+*/ 
+vector<int> kthSmallLarge(vector<int> &arr, int n, int k){
+	// Test 
+	// N = 4
+	// K = 3
+	// arr = { 1 2 4 5 } 
+	sort(arr.begin() , arr.end()) ;
+	return {arr[k - 1] , arr[n - k]} ;
+}
